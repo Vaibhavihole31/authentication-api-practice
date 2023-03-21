f0c2afa6ed7f69a8a068cf83aeeeb55c812ca054
Vaibhavihole31 <itspinki05@gmail.com>
2023-03-21 12:46:17 +0530

    feat: userLogin controller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ class UserController {
         const { name, email, password, password_confirmation, tc } = req.body
         const user = await User.findOne({ email: email })
         if (user) {
-            res.send({ "status": "false", "message": "Email already exists" })
+            res.status(201).send({ "status": "false", "message": "Email already exists" })
         }
         else {
             if (name && email && password && password_confirmation && tc) {
@@ -26,16 +26,44 @@ class UserController {
                     }
                     catch (err) {
                         console.log(err.message);
-                        res.send({ "status": "false", "message": "unable to register" })
+                        res.status(201).send({ "status": "false", "message": "unable to register" })
                     }
                 }
                 else {
-                    res.send({ "status": "false", "message": "password and confirm password doesn't match" })
+                    res.status(201).send({ "status": "false", "message": "password and confirm password doesn't match" })
                 }
             }
             else {
-                res.send({ "status": "false", "message": "All fields are required " })
+                res.status(201).send({ "status": "false", "message": "All fields are required " })
             }
+        }
+    }
+
+    static userLogin = async (req, res) => {
+        try {
+            const { email, password } = req.body
+
+            if (email && password) {
+                const user = await User.findOne({ email: email })
+
+                if (user != null) {
+                    const isMatch = await bcrypt.compare(password, user.password)
+                    if ((user.email === email), isMatch) {
+                        res.send({ "status": "success", data: newUser, "message": "Login Successfully !!✅" })
+                    } else {
+                        res.status(201).send({ "status": "false", "message": "email or password is not valid " })
+                    }
+
+                } else {
+                    res.status(201).send({ "status": "false", "message": "You are not a registered User" })
+                }
+            }
+            else {
+                res.status(201).send({ "status": "false", "message": "All fields are required " })
+            }
+        } catch (err) {
+            console.log(err.message);
+            res.status(201).send({ "status": "false", "message": "Unable to Login" })
         }
     }
 }
